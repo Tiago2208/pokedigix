b749f9653294ae3d21246f4b49c3fcb6396e9308
Tiago Andrade <tiago22900@gmail.com>
2022-07-25 15:28:39 -0400

    att

--- src/main/java/br/com/digix/pokedigix/pokemon/Pokemon.java
+++ src/main/java/br/com/digix/pokedigix/pokemon/Pokemon.java
@@ -2,6 +2,8 @@ package br.com.digix.pokedigix.pokemon;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -26,6 +28,7 @@ public class Pokemon {
     @Column(nullable = false) 
     private double peso;
     
+    @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 15)
     private Genero genero;
     

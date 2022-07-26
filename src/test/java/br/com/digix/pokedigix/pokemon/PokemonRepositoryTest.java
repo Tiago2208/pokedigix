ea9fbfa30740d0a1ec6ae87697cec8e60c187cd4
Tiago Andrade <tiago22900@gmail.com>
2022-07-26 15:45:27 -0400

    att

--- src/test/java/br/com/digix/pokedigix/pokemon/PokemonRepositoryTest.java
+++ src/test/java/br/com/digix/pokedigix/pokemon/PokemonRepositoryTest.java
@@ -4,8 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.ArrayList;
-import java.util.List;
+
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;

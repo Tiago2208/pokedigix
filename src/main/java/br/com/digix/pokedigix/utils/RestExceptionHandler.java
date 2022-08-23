1dea0d60686e310dfe8014da816f24fe45626683
Tiago Andrade <tiago22900@gmail.com>
2022-08-23 13:14:22 -0400

    att

--- src/main/java/br/com/digix/pokedigix/utils/RestExceptionHandler.java
+++ src/main/java/br/com/digix/pokedigix/utils/RestExceptionHandler.java
@@ -1,17 +1,33 @@
 package br.com.digix.pokedigix.utils;
 
-import org.springframework.http.HttpHeaders;
+import java.time.Instant;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-public class RestExceptionHandler extends ResponseEntityExceptionHandler {
-    @Override
-    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
-            HttpStatus status, WebRequest request) {
-        // TODO Auto-generated method stub
-        return super.handleExceptionInternal(ex, body, headers, status, request);
+@ControllerAdvice
+public class RestExceptionHandler {
+
+    @ExceptionHandler(value = { Exception.class })
+    public ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) {
+        return getExceptionResponseEntity(ex, HttpStatus.INTERNAL_SERVER_ERROR, request);
     }
-        
+
+    private ResponseEntity<Object> getExceptionResponseEntity(final Exception exception,
+            final HttpStatus status,
+            final WebRequest request) {
+        final Map<String, Object> body = new LinkedHashMap<>();
+        final String path = request.getDescription(false);
+        body.put("timestamp", Instant.now());
+        body.put("path", path);
+        body.put("status", status.value());
+        body.put("error", exception.getClass());
+        body.put("message", exception.getMessage());
+        return ResponseEntity.status(status).body(body);
+    }
 }
